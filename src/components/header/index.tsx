735cea1e26c7901d8d6484d696be6c4ef40302ff
TomoriComAncora <lucas.fernando@ufms.br>
2025-11-17 16:55:58 -0400

    Refactor code structure for improved readability and maintainability

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/logo.jpeg";
 import { CircleUserRound, LogIn } from "lucide-react";
 
 export function Header() {
-  const singed = false;
+  const singed = true;
   const loginAuth = false;
 
   return (
-    <div className="w-full flex items-center justify-center h-16 bg-primary">
-      <header>
+    <div className="w-full flex items-center justify-center h-16 bg-primary drop-shadow mb-4">
+      <header className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto">
         <Link to={"/"}>
-          <img src={Logo} alt="Logo do sistema" className="w-52"/>
+          <img
+            src={Logo}
+            alt="Logo do sistema"
+            className="w-16"
+          />
         </Link>
-        <Link to={"/dashboard"}>
-          <CircleUserRound />
-        </Link>
+        {!loginAuth && singed && (
+          <Link to={"/dashboard"}>
+            <CircleUserRound />
+          </Link>
+        )}
+        {!loginAuth && !singed && (
+          <Link to={"/login"}>
+            <LogIn />
+          </Link>
+        )}
       </header>
     </div>
   );
